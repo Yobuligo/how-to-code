34ce9bac476fa3b0484e78945ae73b451f66917e
Peter <hoffmannpeter82@gmail.com>
2023-11-26 14:42:56 +0100

    Initial commit

---
+++ dist/Playground.d.ts
@@ -0,0 +1,9 @@
+declare const firstname: string;
+declare let lastname: string;
+declare const age: number;
+declare const first: number;
+declare const second: number;
+declare const sum: number;
+declare const fullName: string;
+declare function add(first: number, second: number): number;
+declare function calc(operation: string, first: number, second: number): number;
---
+++ src/Playground.ts
@@ -0,0 +1,92 @@
+const firstname: string = "Stacey";
+
+let lastname = "Starfish";
+lastname = "Ant";
+
+const age: number = 35;
+
+const first: number = 1016273612836;
+const second: number = 1167361732;
+const sum: number = first / second;
+
+console.log(sum);
+console.log("Hello World");
+console.log(123);
+
+const fullName = firstname + " " + lastname + " ...";
+console.log(fullName);
+console.log(`Hello World ${firstname} ${lastname}.`);
+
+function add(first: number, second: number): number {
+  //  const sum: number = first + second;
+  return first + second;
+}
+
+console.log(add(10, 12));
+console.log(add(22, 23));
+console.log(add(5, 6));
+
+if (first === second) {
+  console.log("sind gleich");
+} else {
+  console.log("sind ungleich");
+}
+
+if (first !== second) {
+  console.log("sind ungleich");
+} else {
+  console.log("sind gleich");
+}
+
+if (first > second) {
+  console.log("ist größer als");
+} else {
+  console.log("ist kleiner als");
+}
+
+if (first < second) {
+  console.log("ist kleiner als");
+} else {
+  console.log("ist größer als");
+}
+
+if (first >= second) {
+  console.log("ist größer als");
+} else {
+  console.log("ist kleiner als");
+}
+
+if (first <= second) {
+  console.log("ist kleiner als");
+} else {
+  console.log("ist größer als");
+}
+
+function calc(operation: string, first: number, second: number): number {
+  if (operation === "*") {
+    return first * second;
+  }
+
+  if (operation === "/") {
+    return first / second;
+  }
+
+  if (operation === "+") {
+    return first + second;
+  }
+
+  if (operation === "-") {
+    return first - second;
+  }
+
+  throw new Error("Invalid operation");
+}
+
+console.log(calc("*", 5, 5));
+console.log(calc("-", 5, 5));
+console.log(calc("+", 5, 5));
+console.log(calc("/", 5, 5));
+
+for (let i = 0; i < 10; i = i + 1) {
+  console.log(i);
+}
